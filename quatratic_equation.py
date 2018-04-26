c836ae8dbfa41e3cfd60ac3e36cddc8860208adc
Abhimanyu Haralukallu <32696360+Abhimamyu@users.noreply.github.com>
2018-04-26 17:30:34 +0530

    Add files via upload

---
+++ quatratic_equation.py
@@ -0,0 +1,32 @@
+#!/bin/python 
+
+# a*x^2 + b*x + c = 0
+
+a = input('Enter X^2 coeff (a) : ')
+b = input('Enter X coeff (b) : ')
+c = input('Enter constant C : ')
+
+m = a*c
+
+if(m < 0):
+    r = range(m, 0)
+elif(b < 0):
+    r = range(-m, m+1)
+else:
+    r = range(1,m+1)
+
+print "\n No. of solutions : "
+is_not_exist = True
+
+for x in r: 
+     if (x != 0):
+         # print m/x
+         if(m%x==0):
+             y = m/x
+             # print y
+             if(b ==(x + y)):
+                 is_not_exist = False
+                 print "(" + str(x) + ", " + str(y) + ")"
+
+if(is_not_exist): print "No solution exist"
+
